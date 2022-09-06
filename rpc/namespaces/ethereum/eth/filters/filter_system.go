95ddfa251484e7954c8faeab5a55760d3b659a53
mmsqe <mavis@crypto.com>
2022-09-06 09:26:27 +0800

    avoid send data to channel when context cancelled

--- rpc/namespaces/ethereum/eth/filters/filter_system.go
+++ rpc/namespaces/ethereum/eth/filters/filter_system.go
@@ -177,7 +177,9 @@ func (es *EventSystem) subscribe(sub *Subscription) (*Subscription, pubsub.Unsub
 				}
 				res.Items = items
 				sub.after = newest
-				eventCh <- res
+				if ctx.Err() == nil {
+					eventCh <- res
+				}
 			}
 		}
 	}()
