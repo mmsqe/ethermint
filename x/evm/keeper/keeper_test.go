10bba9dadaa4211556260a028e0692979f7efde1
mmsqe <mavis@crypto.com>
2022-10-22 07:58:29 +0800

    fix(evm,rpc): coinbase should not be the current one in traceTransaction execution (#1392)
    * add proposer address
    
    * make proto-all
    
    * update nix
    
    * fix test
    
    * keep default proposerAddress
    
    * add change doc
    
    * refine GetProposerAddress with test
    
    * include ProposerAddress for trace api
    
    * fix eth call req
    
    * wrap proposerAddress for eth call
    
    * allow proto translates to sdk.ConsAddress
    
    * Update rpc/backend/call_tx.go
    
    Co-authored-by: Freddy Caceres <facs95@gmail.com>
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>

--- x/evm/keeper/keeper_test.go
+++ x/evm/keeper/keeper_test.go
@@ -254,10 +254,10 @@ func (suite *KeeperTestSuite) DeployTestContract(t require.TestingT, owner commo
 		Data: (*hexutil.Bytes)(&data),
 	})
 	require.NoError(t, err)
-
 	res, err := suite.queryClient.EstimateGas(ctx, &types.EthCallRequest{
-		Args:   args,
-		GasCap: uint64(config.DefaultGasCap),
+		Args:            args,
+		GasCap:          uint64(config.DefaultGasCap),
+		ProposerAddress: suite.ctx.BlockHeader().ProposerAddress,
 	})
 	require.NoError(t, err)
 
@@ -305,8 +305,9 @@ func (suite *KeeperTestSuite) TransferERC20Token(t require.TestingT, contractAdd
 	args, err := json.Marshal(&types.TransactionArgs{To: &contractAddr, From: &from, Data: (*hexutil.Bytes)(&transferData)})
 	require.NoError(t, err)
 	res, err := suite.queryClient.EstimateGas(ctx, &types.EthCallRequest{
-		Args:   args,
-		GasCap: 25_000_000,
+		Args:            args,
+		GasCap:          25_000_000,
+		ProposerAddress: suite.ctx.BlockHeader().ProposerAddress,
 	})
 	require.NoError(t, err)
 
@@ -362,8 +363,9 @@ func (suite *KeeperTestSuite) DeployTestMessageCall(t require.TestingT) common.A
 	require.NoError(t, err)
 
 	res, err := suite.queryClient.EstimateGas(ctx, &types.EthCallRequest{
-		Args:   args,
-		GasCap: uint64(config.DefaultGasCap),
+		Args:            args,
+		GasCap:          uint64(config.DefaultGasCap),
+		ProposerAddress: suite.ctx.BlockHeader().ProposerAddress,
 	})
 	require.NoError(t, err)
 
